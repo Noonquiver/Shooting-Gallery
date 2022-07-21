f916e75bb066df0ed1c6118290ac04b5d7876a46
noonquiver <camilo.hernandez25@hotmail.com>
2022-07-20 20:42:34 -0500

    Targets are clickable,add or substract score and dissapear on click.

--- Shooting Gallery/GameScene.swift
+++ Shooting Gallery/GameScene.swift
@@ -11,15 +11,30 @@ import GameplayKit
 class GameScene: SKScene {
     var possibleTargets = ["target", "badTarget"]
     var timer: Timer?
+    var scoreLabel: SKLabelNode!
+    var score = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
     
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "background")
+        background.name = "background"
         background.zPosition = -1
         background.position = CGPoint(x: 512, y: 384)
         background.scale(to: CGSize(width: 1024, height: 768))
+        background.blendMode = .replace
         addChild(background)
         
-        timer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(createTarget), userInfo: nil, repeats: true)
+        scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        scoreLabel.text = "Score: 0"
+        scoreLabel.position = CGPoint(x: 8, y: 8)
+        scoreLabel.horizontalAlignmentMode = .left
+        scoreLabel.fontSize = 48
+        addChild(scoreLabel)
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(createTarget), userInfo: nil, repeats: true)
     }
 
     override func update(_ currentTime: TimeInterval) {
@@ -33,19 +48,42 @@ class GameScene: SKScene {
     @objc func createTarget() {
         guard let target = possibleTargets.randomElement() else { return }
         let randomRowNumber = Int.random(in: 1...3)
-        let randomSize = Int.random(in: 1...5)
+        let randomSize = Int.random(in: 1...3)
         
         let sprite = SKSpriteNode(imageNamed: target)
         sprite.size = CGSize(width: 30 * randomSize, height: 30 * randomSize)
         sprite.physicsBody = SKPhysicsBody(texture: sprite.texture!, size: sprite.size)
         
         switch randomRowNumber {
+        case 1:
+            sprite.position = CGPoint(x: -136, y: 200 * randomRowNumber)
+            sprite.physicsBody?.velocity = CGVector(dx: 200, dy: 0)
+            
         case 2:
             sprite.position = CGPoint(x: 1200, y: 200 * randomRowNumber)
-            sprite.physicsBody?.velocity = CGVector(dx: -500, dy: 0)
-        default:
+            sprite.physicsBody?.velocity = CGVector(dx: -200, dy: 0)
+            
+        case 3:
             sprite.position = CGPoint(x: -136, y: 200 * randomRowNumber)
-            sprite.physicsBody?.velocity = CGVector(dx: 500, dy: 0)
+            sprite.physicsBody?.velocity = CGVector(dx: 200, dy: 0)
+        default: break
+        }
+        
+        switch randomSize {
+        case 1: sprite.name = "small"
+        case 2: sprite.name = "medium"
+        case 3: sprite.name = "big"
+        default: break
+        }
+        
+        switch target {
+        case "target": break
+        case "badTarget":
+            if var name = sprite.name {
+                name += "Bad"
+                sprite.name = name
+            }
+        default: break
         }
         
         addChild(sprite)
@@ -59,7 +97,35 @@ class GameScene: SKScene {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let touch = touches.first else { return }
         let location = touch.location(in: self)
+        let tappedNodes = nodes(at: location)
         
         
+        for node in tappedNodes {
+            guard let isBadTarget = node.name?.contains("Bad") else { return }
+            guard let isSmall = node.name?.contains("small") else { return }
+            guard let isMedium = node.name?.contains("medium") else { return }
+            
+            if node.name != "background" {
+                if isBadTarget {
+                    if isSmall {
+                        score -= 100
+                    } else if isMedium {
+                        score -= 200
+                    } else {
+                        score -= 300
+                    }
+                } else {
+                    if isSmall {
+                        score += 300
+                    } else if isMedium {
+                        score += 200
+                    } else {
+                        score += 100
+                    }
+                }
+                
+                node.removeFromParent()
+            }
+        }
     }
 }
